a9d49bd244d718730f9a4a8d872fc0cb7c2a4bf9
pooyan <thepooyan@gmail.com>
2025-06-23 21:07:28 +0330

    nothing!

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,4 @@
-import { action, redirect, useSubmission } from "@solidjs/router"
+import { action, createAsync, json, query, redirect, useSubmission } from "@solidjs/router"
 import { createEffect } from "solid-js"
 import { getRequestEvent } from "solid-js/web"
 import { Button } from "~/components/ui/button"
@@ -18,19 +18,44 @@ const loginAction = action(async (formData:FormData)=> {
   return redirect("/")
 })
 
+const getGoogleProvider = query(async () => {
+  "use server"
+  const event = getRequestEvent()
+  if (!event) throw new Error("no event")
+  let methods = await event.locals.pb.collection("users").listAuthMethods()
+  let google = methods.oauth2.providers.find(p => p.name === "google")
+  if (!google) throw new Error("google not set")
+  return json(google, {revalidate: "nothing"})
+},"googleAuth")
+
 const login = () => {
+  const googleProvider = createAsync(() => getGoogleProvider())
+
   const sub = useSubmission(loginAction)
   createEffect(() => {
     console.log(sub.result)
   })
+
+  const googleAuth = () => {
+    const provider = googleProvider()
+    if (!provider) return
+    let href = provider.authURL + "http://localhost:3000/googleRedirect"
+    localStorage.setItem("provider", JSON.stringify(provider));
+    window.open(href)
+  }
+
   return (
-    <form class=" m-auto w-2xl space-y-2 mt-10 " method="post" action={loginAction}>
-      Email:
-      <Input name="email"/>
-      Password:
-      <Input name="pass"/>
-      <Button type="submit">Done</Button>
-    </form>
+    <>
+      <form class=" m-auto w-2xl space-y-2 mt-10 " method="post" action={loginAction}>
+        Email:
+        <Input name="email"/>
+        Password:
+        <Input name="pass"/>
+        <Button type="submit">Done</Button>
+      </form>
+
+      <Button onclick={googleAuth}>Continue with Google</Button>
+    </>
   )
 }
 
