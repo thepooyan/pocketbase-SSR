9a31b70079dd59cd8f5b2d52b6b0ffb04925342a
pooyan <thepooyan@gmail.com>
2025-06-23 15:14:08 +0330

    nothing!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
 import { createAsync, json, query } from "@solidjs/router";
-import { getRequestEvent } from "solid-js/web";
+import { getRequestEvent, Show } from "solid-js/web";
 import { Button } from "~/components/ui/button";
 
 const auth = query(async () => {
   "use server"
   const event = getRequestEvent()
   if (!event) return
-  console.log(event.locals.pb.authStore.isValid);
-  console.log(event.locals.pb.authStore.token);
-  console.log(event.locals.pb.authStore.record.id);
+
   return json({
     isValid: event.locals.pb.authStore.isValid
   })
@@ -16,9 +14,13 @@ const auth = query(async () => {
 
 export default function Home() {
   const user = createAsync(() => auth())
-  console.log(user())
+  console.log(user()?.isValid)
   return (
     <>
+      <Show when={user()?.isValid === true}>
+        Welcome!
+      </Show>
+      <br/>
       <Button as="A" href="/login">Login</Button>
     </>
   );
