6dcc73f91c730710df95e66033a459c845711e7c
pooyan <thepooyan@gmail.com>
2025-06-23 21:18:53 +0330

    nothing!

---
+++ src/routes/googleRedirect.tsx
@@ -0,0 +1,33 @@
+import { action, redirect, useAction, useSearchParams } from "@solidjs/router"
+import { onMount } from "solid-js"
+import { getRequestEvent } from "solid-js/web"
+
+const folan = action(async (provider: string, code: any, codeVerifier: string, redirectUrl: string) => {
+  "use server"
+  const event = getRequestEvent()
+  if (!event) return
+  try {
+    await event.locals.pb.collection("users").authWithOAuth2Code(provider, code, codeVerifier, redirectUrl)
+  } catch(e) {
+    console.log(e)
+    return {ok: false}
+  }
+  throw redirect("/")
+})
+
+const googleRedirect = () => {
+  const action = useAction(folan)
+  const [params, _] = useSearchParams()
+
+  onMount(() => {
+    const provider = JSON.parse(localStorage.getItem("provider") || "")
+    if (provider.state !== params.state) throw new Error("State don't match")
+    action(provider.name, params.code, provider.codeVerifier, "http://localhost:3000/googleRedirect")
+  })
+
+  return (
+    <>Authenticating...</>
+  )
+}
+
+export default googleRedirect
